baf66b22a0719f3f49ecc0ba83c7d5bdf261b61b
ulises-m <ulises_cruz_19@outlook.com>
2013-12-08 21:03:28 -0600

    Initial commit

---
+++ Practica7y8/Practica7y8/MySQL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+
+using MySql.Data.MySqlClient;
+namespace Practica7
+{
+	public class MySQL
+	{
+		protected MySqlConnection myConnection;
+		string connectionString;
+		public MySQL()
+		{
+		}
+		protected void abrirConexion(){
+			connectionString = 
+				"Server=localhost;" +
+				"Database=practica7;" +
+				"Uid=root;" +
+				"Pwd=2681124g;"+
+				"Pooling=false;";
+			this.myConnection = new MySqlConnection (connectionString);
+			this.myConnection.Open();
+		}
+		public void cerrarConexion(){
+		
+			this.myConnection.Close();
+			this.myConnection = null;
+		
+		}
+	}
+}
